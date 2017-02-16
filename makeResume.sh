783969233da33b12aa61ac3319a9e49755e0d4c1
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2017-02-16 16:25:05 -0700

    moved yaml config to separate file

--- makeResume.sh
+++ makeResume.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Use input file resume.md and output resume.pdf (now using yaml metadata for formatting)
-pandoc resume.md -o resume.pdf
+pandoc resume.md -s formatting.yaml -o resume.pdf
