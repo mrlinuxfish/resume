72e5a76ac4ea2a3e4941276becfea49b490c663e
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2017-02-27 11:08:09 -0700

    Add support for coverletters

--- makeResume.sh
+++ makeResume.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# Use input file resume.md and output resume.pdf (now using yaml metadata for formatting)
-pandoc resume.md -s formatting.yaml -o resume.pdf
+# Test if there is a file input
+case "$*" in
+	# Show help
+	(*-h*) echo "run the script with -c to specify a coverletter in the coverletter directory" ;;
+
+	# Pull the second argument from makeResume.sh and use it a the coverletter
+	(*-c*) pandoc coverletter/$2.md coverletter/format.md resume.md -s formatting.yaml -o coverletter/$2-resume.pdf ;;
+
+	# If there is no argument make the resume in the root of this directory
+	(*) pandoc resume.md -s formatting.yaml -o resume.pdf ;;
+esac
