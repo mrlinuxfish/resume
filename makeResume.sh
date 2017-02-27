8c6ef6270b9de75b77a77a3088655570d33d150d
crazylegoguy@gmail.com <crazylegoguy@gmail.com>
2017-02-27 12:18:19 -0700

    Make links blue

--- makeResume.sh
+++ makeResume.sh
@@ -6,8 +6,8 @@ case "$*" in
 	(*-h*) echo "run the script with -c to specify a coverletter in the coverletter directory" ;;
 
 	# Pull the second argument from makeResume.sh and use it a the coverletter
-	(*-c*) pandoc coverletter/$2.md coverletter/format.md resume.md -s formatting.yaml -o coverletter/$2-resume.pdf ;;
+	(*-c*) pandoc coverletter/$2.md coverletter/format.md resume.md -s formatting.yaml --variable urlcolor=cyan -o coverletter/$2-resume.pdf ;;
 
 	# If there is no argument make the resume in the root of this directory
-	(*) pandoc resume.md -s formatting.yaml -o resume.pdf ;;
+	(*) pandoc resume.md -s formatting.yaml --variable urlcolor=cyan -o resume.pdf ;;
 esac
